44adca8e3a1b674ff8babfb1c0e10681d4aa5727
brianzammit <brian.zammit.09@gmail.com>
2014-11-10 22:14:57 +0100

    Created User Validator
    Created (not implemented UserValidator

---
+++ src/main/java/validators/UserValidator.java
@@ -0,0 +1,18 @@
+package validators;
+
+import persistant.User;
+
+public class UserValidator {
+
+	public boolean validateUser(User user)
+	{
+		return false;
+		
+	}
+	
+	public boolean validatePassword(String password)
+	{
+		return false;
+		
+	}
+}
