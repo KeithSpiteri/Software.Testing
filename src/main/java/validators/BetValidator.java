8bea7ac85c59f3419404e7d6ffac4742f7345967
brianzammit <brian.zammit.09@gmail.com>
2015-01-02 21:48:32 +0100

    Minor changes

--- src/main/java/validators/BetValidator.java
+++ src/main/java/validators/BetValidator.java
@@ -8,11 +8,10 @@ import persistant.User;
 
 public class BetValidator {
 
-	public boolean validateBet(User user, Bet bet)
-	{
+	public boolean validateBet(User user, Bet bet) {
 		return validateRiskLevel(user, bet) && validateAmount(user, bet);
 	}
-	
+
 	public boolean validateRiskLevel(User user, Bet bet) {
 		if (!bet.getRiskLevel().equals("low")
 				&& !bet.getRiskLevel().equals("medium")
@@ -27,27 +26,29 @@ public class BetValidator {
 
 	public boolean validateAmount(User user, Bet bet) {
 		DbService dbService = DbService.getInstance();
+		if (bet.getAmount() > 0) {
+			if (user.isFree()) {
+				if (bet.getAmount() > 5)
+					return false;
 
-		if (user.isFree()) {
-			if (bet.getAmount() > 5)
-				return false;
-
-			if (dbService.countBets(user) >= 3)
-				return false;
-		}
-		// Test for Premium User bets
-		else {
-			List<Bet> userBets = (List<Bet>)dbService.getUserBets(user);
-
-			double total = bet.getAmount();
-			for (Object o : userBets) {
-				Bet prevBet = (Bet)o;
-				total += prevBet.getAmount();
+				if (dbService.countBets(user) >= 3)
+					return false;
 			}
+			// Test for Premium User bets
+			else {
+				List<Bet> userBets = (List<Bet>) dbService.getUserBets(user);
 
-			if (total > 5000)
-				return false;
-		}
+				double total = bet.getAmount();
+				for (Object o : userBets) {
+					Bet prevBet = (Bet) o;
+					total += prevBet.getAmount();
+				}
+
+				if (total > 5000)
+					return false;
+			}
+		} else
+			return false;
 
 		return true;
 	}
